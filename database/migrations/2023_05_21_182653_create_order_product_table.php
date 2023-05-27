a0966cc838bff4ac25f812cf64f6bb4ef43bf69d
abdolvahedsafizadeh <vahed@hotmail.co.uk>
2023-05-27 20:35:22 +0100

    update migration

--- database/migrations/2023_05_21_182653_create_order_product_table.php
+++ database/migrations/2023_05_21_182653_create_order_product_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('order_id')->unsigned()->nullable();
-            $table->foreign('order_id')->references('id')->on('orders')->onUpdate('cascade')->onDelete('set null');
+            //$table->foreign('order_id')->references('id')->on('orders')->onUpdate('cascade')->onDelete('set null');
             $table->unsignedBigInteger('product_id')->unsigned()->nullable();
-            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('set null');
+            //$table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('set null');
             $table->integer('quantity')->unsigned();
             $table->timestamps();
         });
